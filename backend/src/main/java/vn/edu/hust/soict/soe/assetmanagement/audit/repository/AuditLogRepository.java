c389597669d13280a927d3b6cae803a8ae7551e9
Le Viet Cuong <cle15102005@gmail.com>
2026-06-08 11:35:56 +0700

    fix use case

--- backend/src/main/java/vn/edu/hust/soict/soe/assetmanagement/audit/repository/AuditLogRepository.java
+++ backend/src/main/java/vn/edu/hust/soict/soe/assetmanagement/audit/repository/AuditLogRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 import vn.edu.hust.soict.soe.assetmanagement.audit.entity.AuditLog;
 
 import java.time.LocalDateTime;
